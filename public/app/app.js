e3660b08c3ecd836529c8bc682ead996f1666f9a
samipshrestha <basantaoffice@gmail.com>
2016-03-04 11:25:59 +0545

    Front end  angular js and html functionalities

---
+++ public/app/app.js
@@ -0,0 +1,2 @@
+var app = angular.module('clientList', [])
+        .constant('API_URL', 'http://localhost:8000/api/v1/');
---
+++ public/app/controllers/clients.js
@@ -0,0 +1,47 @@
+app.controller('clientsController', function($scope, $http, API_URL) {
+
+    //retrieve clients listing from API
+    $http.get(API_URL + "client")
+            .success(function(response) {
+                $scope.clients = response;
+
+            });
+    
+    //show modal form
+    $scope.toggle = function(modalstate) {
+        $scope.modalstate = modalstate;
+
+        switch (modalstate) {
+            case 'add':
+                $scope.form_title = "Add New Client";
+                break;
+            
+            default:
+                break;
+        }
+        
+        $('#myModal').modal('show');
+    }
+
+    //save new record 
+    $scope.save = function(modalstate) {
+        var url = API_URL + "client";
+        
+        $http({
+            method: 'POST',
+            url: url,
+            data: $.param($scope.client),
+
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function(response) {
+            console.log(response);
+            location.reload();
+        }).error(function(response) {
+            console.log(response);
+            alert('This is embarassing. An error has occured. Please check the log for details');
+            
+        });
+    }
+
+    
+});
